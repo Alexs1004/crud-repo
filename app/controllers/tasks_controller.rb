e8cc17736f9ce651297dbb2103c50852f7fe1fb6
Alexandre Stutz <alexandre.stutz@hotmail.com>
2021-08-03 17:30:53 +0200

    gg

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   end
   
   def show
-    @task = Task.find(task_params)
+    @task = Task.find(params["id"])
   end
 
   def new
@@ -12,18 +12,19 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(params[:task])
+    @task = Task.new(task_params)
     @task.save
+    redirect_to tasks_path
   end
 
   def edit
-    @task = Task.find(task_params)
+    @task = Task.find(params["id"])
   end
   
 
   def update
-    @task = Task.find(task_params)
-    @task.update(params[:task])
+    @task = Task.find(params["id"])
+    @task.update(task_params)
     redirect_to task_path(@task)
   end
 
@@ -32,10 +33,11 @@ class TasksController < ApplicationController
     @task.destroy
     redirect_to tasks_path
   end
-
+  
   private
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
 end
+  
